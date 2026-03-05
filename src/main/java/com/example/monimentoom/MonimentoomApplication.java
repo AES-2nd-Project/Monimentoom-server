6c5721913fd7cc3d28f1b837d44eb0f4f423aa17
imyuri <dladbfl67@naver.com>
2026-03-05 18:45:40 +0900

    Initial commit

---
+++ src/main/java/com/example/monimentoom/MonimentoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.monimentoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonimentoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonimentoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/monimentoom/MonimentoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monimentoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonimentoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
