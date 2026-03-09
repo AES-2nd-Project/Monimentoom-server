71906f75e8b0d02081cd8816a3d72277003a8601
imyuri <dladbfl67@naver.com>
2026-03-09 17:27:07 +0900

    feat: CORS vercel 허용 추가

--- src/main/java/com/example/monimentoom/config/WebConfig.java
+++ src/main/java/com/example/monimentoom/config/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://monimentoom-frontend.vercel.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
