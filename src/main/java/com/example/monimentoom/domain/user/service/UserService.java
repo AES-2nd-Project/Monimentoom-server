cbda5a8c944d8bdb836fe62c358642f0d220ad6f
glyph8 <newdy0808@gmail.com>
2026-03-09 11:35:31 +0900

    feat : spring security 활성화, 비밀번호 해싱하여 회원가입, 로그인 구현

--- src/main/java/com/example/monimentoom/domain/user/service/UserService.java
+++ src/main/java/com/example/monimentoom/domain/user/service/UserService.java
@@ -5,15 +5,16 @@ import com.example.monimentoom.domain.user.dto.UserSignupRequest;
 import com.example.monimentoom.domain.user.model.User;
 import com.example.monimentoom.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void createUser(UserSignupRequest request) {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
         User user = User.builder()
                 .email(request.getEmail())
@@ -24,9 +25,6 @@ public class UserService {
     }
 
     public boolean loginUser(UserLoginRequest request){
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(request.getPassword());
-        request.setPassword(encodedPassword);
         User user = userRepository.findByEmail(request.getEmail())
                     .orElseThrow(() -> new IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다"));
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
