ec44c1130de8499526c2b005309e85c02e806ef2
imyuri <dladbfl67@naver.com>
2026-03-09 20:59:22 +0900

    fix: 코파일럿 리뷰 반영

--- src/main/java/com/example/monimentoom/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/monimentoom/exception/GlobalExceptionHandler.java
@@ -1,15 +1,12 @@
 package com.example.monimentoom.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = new ErrorResponse(
