82000c8ed14b56d0352cfba001953de9acfe0d6a
imyuri <dladbfl67@naver.com>
2026-03-10 16:31:23 +0900

    fix: 인증 우회용 로컬 security 파일 작성

--- src/main/java/com/example/monimentoom/config/SecurityConfigLocal.java
+++ src/main/java/com/example/monimentoom/config/SecurityConfigLocal.java
@@ -14,11 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Profile("!local")
+@Profile("local")
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfigLocal {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
@@ -35,9 +35,9 @@ public class SecurityConfig {
                 .httpBasic(basic -> basic.disable())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/users/login", "/users/signup", "/error",
-                                        "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                                .anyRequest().authenticated()
+                        .requestMatchers("/users/login", "/users/signup", "/error",
+                                "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
