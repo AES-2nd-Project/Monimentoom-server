379034d47b81f226c1c91781227fa7c8cfe263ad
imyuri <dladbfl67@naver.com>
2026-03-10 10:12:13 +0900

    feat: Comment 엔티티 작성

--- src/main/java/com/example/monimentoom/domain/comments/domain/Comment.java
+++ src/main/java/com/example/monimentoom/domain/comments/domain/Comment.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @NoArgsConstructor
 @Getter
@@ -24,4 +26,21 @@ public class Comment {
 
     @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
