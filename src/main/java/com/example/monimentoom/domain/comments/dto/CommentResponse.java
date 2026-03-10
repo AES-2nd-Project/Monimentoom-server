0cd15c818c688d01a8242c49e7daeb1ff1a1eb34
imyuri <dladbfl67@naver.com>
2026-03-10 17:38:36 +0900

    feat: CommentResponse의 createdAt 응답을 파싱

--- src/main/java/com/example/monimentoom/domain/comments/dto/CommentResponse.java
+++ src/main/java/com/example/monimentoom/domain/comments/dto/CommentResponse.java
@@ -1,6 +1,7 @@
 package com.example.monimentoom.domain.comments.dto;
 
 import com.example.monimentoom.domain.comments.model.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,8 +15,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CommentResponse {
     private String nickname;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
-
     private String content;
 
     public static CommentResponse from(Comment comment) {
