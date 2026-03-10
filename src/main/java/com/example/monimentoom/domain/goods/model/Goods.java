447e4228b31ea065d516ba317107010f143333b2
imyuri <dladbfl67@naver.com>
2026-03-10 10:53:08 +0900

    feat: createdAt, updatedAt 모든 엔티티에 추가

--- src/main/java/com/example/monimentoom/domain/goods/model/Goods.java
+++ src/main/java/com/example/monimentoom/domain/goods/model/Goods.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -31,8 +32,24 @@ public class Goods {
     private String description;
     private Integer price;
 
-    @OneToMany(mappedBy = "goods", cascade = CascadeType.ALL, orphanRemoval = true , fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "goods", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Position> positions = new ArrayList<>();
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     @Builder
     public Goods(User user, String name, String imageUrl, String description, Integer price, List<Position> positions) {
--- src/main/java/com/example/monimentoom/domain/user/User.java
+++ src/main/java/com/example/monimentoom/domain/user/User.java
@@ -3,13 +3,16 @@ package com.example.monimentoom.domain.user;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Table(name = "users", indexes = @Index(name="idx_nickname", columnList = "nickname"))
+@Table(name = "users", indexes = @Index(name = "idx_nickname", columnList = "nickname"))
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
@@ -18,6 +21,22 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     @Builder
     public User(String nickname, String email, String password) {
