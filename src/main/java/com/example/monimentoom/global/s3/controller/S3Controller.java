6fa62e737e302e1246955b3fa5f0c785923571a7
imyuri <dladbfl67@naver.com>
2026-03-12 14:09:35 +0900

    refactor: s3 api 경로 수정

--- src/main/java/com/example/monimentoom/global/s3/controller/S3Controller.java
+++ src/main/java/com/example/monimentoom/global/s3/controller/S3Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/s3")
+@RequestMapping("/s3")
 @RequiredArgsConstructor
 public class S3Controller {
     private final S3Uploader s3Uploader;
