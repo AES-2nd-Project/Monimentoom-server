c8f021adcfdf7c18e5f193bd449fb60fb63fbb05
imyuri <dladbfl67@naver.com>
2026-03-13 14:52:23 +0900

    feat: 내가 좋아요한 방들 불러오기

--- src/main/java/com/example/monimentoom/domain/like/controller/LikeController.java
+++ src/main/java/com/example/monimentoom/domain/like/controller/LikeController.java
@@ -2,11 +2,14 @@ package com.example.monimentoom.domain.like.controller;
 
 import com.example.monimentoom.domain.like.dto.LikeResponse;
 import com.example.monimentoom.domain.like.service.LikeService;
+import com.example.monimentoom.domain.room.dto.RoomBasicResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/likes")
@@ -17,6 +20,11 @@ public class LikeController {
     @GetMapping("/{roomId}")
     public ResponseEntity<LikeResponse> getLikes(@AuthenticationPrincipal Long userId, @PathVariable Long roomId) {
         return ResponseEntity.ok(likeService.getLikes(userId, roomId));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<List<RoomBasicResponse>> getLikedRooms(@AuthenticationPrincipal Long userId) {
+        return ResponseEntity.ok(likeService.getLikedRooms(userId));
     }
 
     @PostMapping("/{roomId}")
--- src/main/java/com/example/monimentoom/domain/like/repository/LikeRepository.java
+++ src/main/java/com/example/monimentoom/domain/like/repository/LikeRepository.java
@@ -1,7 +1,11 @@
 package com.example.monimentoom.domain.like.repository;
 
 import com.example.monimentoom.domain.like.model.Like;
+import com.example.monimentoom.domain.room.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     long countByRoomId(Long roomId);
@@ -9,4 +13,12 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     boolean existsByRoomIdAndUserId(Long roomId, Long userId);
 
     long deleteByUserIdAndRoomId(Long userId, Long roomId);
+
+    @Query("""
+            SELECT l FROM Like l
+            JOIN FETCH l.room
+            JOIN FETCH l.user
+            WHERE l.user.id = :userId
+            """)
+    List<Like> findByUserId(Long userId);
 }
