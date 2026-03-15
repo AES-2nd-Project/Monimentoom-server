79c4c83021b466187ebedf511c238c3d0c96a6d4
imyuri <dladbfl67@naver.com>
2026-03-15 16:30:41 +0900

    feat: mainRoom 수정을 set 대신 update 함수로

--- src/main/java/com/example/monimentoom/domain/user/model/User.java
+++ src/main/java/com/example/monimentoom/domain/user/model/User.java
@@ -5,14 +5,16 @@ import com.example.monimentoom.exception.CustomException;
 import com.example.monimentoom.exception.ErrorCode;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -55,5 +57,9 @@ public class User {
         if (!this.id.equals(userId)) {
             throw new CustomException(ErrorCode.FORBIDDEN);
         }
+    }
+
+    public void updateMainRoom(Room mainRoom) {
+        this.mainRoom = mainRoom;
     }
 }
